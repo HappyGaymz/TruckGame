5b6918caf372a02fdc56baaf65cc3648c00275cd
Amir <amir.ghaffarie@gmail.com>
2022-07-09 19:49:49 +0430

    motor force

--- Assets/MotorForceApplier.cs
+++ Assets/MotorForceApplier.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class MotorForceApplier : MonoBehaviour
 {
+    [SerializeField] private float topSpeed;
     [SerializeField] private float torquePower;
     [SerializeField] private List<WheelCollider> wheels;
+    Rigidbody rb;
+    private float trueTopSpeed;
+    private void Awake()
+    {
+        trueTopSpeed = topSpeed/3.6f;
+        trueTopSpeed = trueTopSpeed * trueTopSpeed;
+        rb = GetComponent<Rigidbody>();
+    }
     private void FixedUpdate()
     {
-        wheels.ForEach((w) => w.motorTorque = torquePower);
+        if(rb.velocity.sqrMagnitude < trueTopSpeed)
+            wheels.ForEach((w) => w.motorTorque = torquePower);
+        else
+            wheels.ForEach((w) => w.motorTorque = 0);
     }
 }
