91c2221b43e244e959d9a13f29313cc0ed8100aa
Amir <amir.ghaffarie@gmail.com>
2022-07-10 20:04:33 +0430

    added auto brake

--- Assets/MotorForceApplier.cs
+++ Assets/MotorForceApplier.cs
@@ -7,21 +7,36 @@ public class MotorForceApplier : MonoBehaviour
     [SerializeField] private float downforce = 50;
     [SerializeField] private float topSpeed;
     [SerializeField] private float torquePower;
+    [SerializeField] private float brakePower;
     [SerializeField] private List<WheelCollider> wheels;
     Rigidbody rb;
     private float trueTopSpeed;
+    private float brakeTopSpeed;
     private void Awake()
     {
         trueTopSpeed = topSpeed/3.6f;
         trueTopSpeed = trueTopSpeed * trueTopSpeed;
+        brakeTopSpeed = trueTopSpeed * 1.5f;
         rb = GetComponent<Rigidbody>();
     }
     private void FixedUpdate()
     {
-        if(rb.velocity.sqrMagnitude < trueTopSpeed)
+        if (rb.velocity.sqrMagnitude < trueTopSpeed)
+        {
             wheels.ForEach((w) => w.motorTorque = torquePower);
+        }
         else
+        {
             wheels.ForEach((w) => w.motorTorque = 0);
+        } 
+        if (rb.velocity.sqrMagnitude < brakeTopSpeed)
+        {
+            wheels.ForEach((w) => w.brakeTorque = 0);
+        }
+        else
+        {
+            wheels.ForEach((w) => w.brakeTorque = brakePower);
+        }
         rb.AddForce(Vector3.down * downforce * rb.velocity.magnitude);
     }
 }
